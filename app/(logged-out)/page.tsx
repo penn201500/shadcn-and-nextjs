894c1409c0877456fd028ba03d336002be0fa61f
penn <penn201500@gmail.com>
2025-01-07 18:52:26 +0800

    ✨ Add button on landing page
    true

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 export default function LandingPage() {
     return (
         <div>
             <h1>hello world</h1>
+            <Button>
+                Click me
+            </Button>
         </div>
     )
 }
