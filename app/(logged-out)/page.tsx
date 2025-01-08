a3feb4083c1d40100853e62c6b3fb963bd074e34
penn <penn201500@gmail.com>
2025-01-08 22:02:52 +0800

    ✨ Add lucid-reacticons and align the elements in page
    true

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@/components/ui/button"
+import { PersonStandingIcon } from "lucide-react"
 
 export default function LandingPage() {
     return (
         <>
-            <h1>Shadcn UI</h1>
-            <Button>Log In</Button>
-            <Button>Sign Up</Button>
+            <h1 className="flex gap-2 items-center">
+                <PersonStandingIcon
+                    size={45}
+                    className="text-pink-500"
+                />{" "}
+                Shadcn UI
+            </h1>
+            <p>The best dashboard to manage customer support</p>
+            <div className="flex items-center gap-2 mt-4">
+                <Button>Log In</Button>
+                <Button>Sign Up</Button>
+            </div>
         </>
     )
 }
