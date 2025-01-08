0da7b37b9f4280d479ab675bfded76cf69cb22ae
penn <penn201500@gmail.com>
2025-01-08 18:17:32 +0800

    👔 Add one button and update the title
    true

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -2,11 +2,10 @@ import { Button } from "@/components/ui/button"
 
 export default function LandingPage() {
     return (
-        <div>
-            <h1>hello world</h1>
-            <Button>
-                Click me
-            </Button>
-        </div>
+        <>
+            <h1>Shadcn UI</h1>
+            <Button>Log In</Button>
+            <Button>Sign Up</Button>
+        </>
     )
 }
