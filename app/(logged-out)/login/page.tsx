e3903de981cc5c436673507489c336f997d4671b
penn <penn201500@gmail.com>
2025-01-09 22:10:23 +0800

    ✨ Add login card
    true

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,5 +1,42 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from "@/components/ui/card"
+import { PersonStandingIcon } from "lucide-react"
+import Link from "next/link"
+
 export default function LoginPage() {
     return (
-        <div>login</div>
+        <>
+            <PersonStandingIcon size={50} />
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                    <CardDescription>Login to your account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content</p>
+                </CardContent>
+                <CardFooter className="justify-between">
+                    <small>Don&apos;t have an account?</small>
+                    <Button
+                        asChild
+                        variant="outline"></Button>
+                    <Button
+                        asChild
+                        variant="outline"
+                        size="sm">
+                        <Link href="/sign-up">Sign Up</Link>
+                    </Button>
+                </CardFooter>
+            </Card>
+        </>
     )
 }
