0ecd978ee237231779ea13c280d46ed1a73324e2
penn <penn201500@gmail.com>
2025-01-10 21:13:37 +0800

    ✨ Add bare bones of sign-up page
    true

---
+++ app/(logged-out)/sign-up/page.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
+} from "@/components/ui/card"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { PersonStandingIcon } from "lucide-react"
+import Link from "next/link"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+const formSchema = z.object({
+    email: z.string().email(),
+})
+
+function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data)
+}
+
+export default function SignUpPage() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+        },
+    })
+
+    return (
+        <>
+            <PersonStandingIcon size={50} />
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Sign Up</CardTitle>
+                    <CardDescription>Sign up for a new account</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="flex flex-col gap-4">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Email"
+                                                type="email"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <Button type="submit">Sign Up</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter className="justify-between">
+                    <small>Already have an account?</small>
+                    <Button
+                        asChild
+                        variant="outline"></Button>
+                    <Button
+                        asChild
+                        variant="outline"
+                        size="sm">
+                        <Link href="/login">Login</Link>
+                    </Button>
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
