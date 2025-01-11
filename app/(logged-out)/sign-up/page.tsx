ed6895cd849bb5fb6d885a676945e5d7b53ac265
penn <penn201500@gmail.com>
2025-01-11 18:20:47 +0800

    ✨ Add select component for sign up form
    true

--- app/(logged-out)/sign-up/page.tsx
+++ app/(logged-out)/sign-up/page.tsx
@@ -19,6 +19,13 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { PersonStandingIcon } from "lucide-react"
 import Link from "next/link"
@@ -27,6 +34,9 @@ import * as z from "zod"
 
 const formSchema = z.object({
     email: z.string().email(),
+    accountType: z.enum(["personal", "company"]),
+    companyName: z.string().optional(),
+    numberOfEmployees: z.coerce.number().optional(),
 })
 
 function onSubmit(data: z.infer<typeof formSchema>) {
@@ -67,6 +77,28 @@ export default function SignUpPage() {
                                                 {...field}
                                             />
                                         </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="accountType"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Account type</FormLabel>
+                                        <Select onValueChange={field.onChange}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select an account type" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="personal">Personal</SelectItem>
+                                                <SelectItem value="company">Company</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormDescription></FormDescription>
                                         <FormMessage />
                                     </FormItem>
                                 )}
