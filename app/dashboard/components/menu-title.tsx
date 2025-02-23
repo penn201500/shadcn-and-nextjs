8ffb8095ad05e270d57be9b7ed1bf9a543e43e8e
penn <penn201500@gmail.com>
2025-02-23 10:07:15 +0800

    ✨ Add main menu component for side panel
    true

---
+++ app/dashboard/components/menu-title.tsx
@@ -0,0 +1,9 @@
+import { PersonStandingIcon } from "lucide-react"
+
+export default function MenuTitle() {
+    return (
+        <h4 className="flex items-center gap-2">
+            <PersonStandingIcon className="text-primary size-8" /> Shadcn UI
+        </h4>
+    )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,9 @@
+import MainMenu from "./components/main-menu"
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="grid grid-cols-[250px_1fr] h-screen">
-            <div className="bg-muted overflow-auto p-4">side panel</div>
+            <MainMenu />
             <div className="overflow-auto py-2 px-4">
                 <h1>Welcome back!</h1>
                 {children}
