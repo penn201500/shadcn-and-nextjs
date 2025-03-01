58a9e3240a858c337492dd4ed5103ab490839248
penn <penn201500@gmail.com>
2025-03-01 17:34:31 +0800

    ✨ Add stats tabs in dashboard page
    true

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { TabsContent } from "@radix-ui/react-tabs"
+
 export default function DashboardPage() {
     return (
-        <div className="flex flex-col gap-4">
-            <p>Welcome to your dashboard</p>
-        </div>
+        <Tabs>
+            <TabsList>
+                <TabsTrigger value="employees">Employees stats</TabsTrigger>
+                <TabsTrigger value="teams">Teams stats</TabsTrigger>
+            </TabsList>
+            <TabsContent value="employees">Employees stats</TabsContent>
+            <TabsContent value="teams">Teams stats</TabsContent>
+        </Tabs>
     )
 }
