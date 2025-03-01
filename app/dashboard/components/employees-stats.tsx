2e3fd63a770f2c85a5e5fac244c9a225af361c10
penn <penn201500@gmail.com>
2025-03-01 18:06:09 +0800

    ✨ Implement the 1st card in employee stats
    true

--- app/dashboard/components/employees-stats.tsx
+++ app/dashboard/components/employees-stats.tsx
@@ -1,11 +1,36 @@
-import { Card } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { User } from "lucide-react"
+import Link from "next/link"
 
 export default function EmployeesStats() {
     return (
         <div className="grid lg:grid-cols-3 gap-4">
-            <Card>card 1</Card>
-            <Card>card 1</Card>
-            <Card>card 1</Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-base">Total employees</CardTitle>
+                </CardHeader>
+                <CardContent className="flex justify-between items-center">
+                    <div className="flex gap-2">
+                        <User size={24} />
+                        <div className="text-5xl font-bold">100</div>
+                    </div>
+                    <div>2</div>
+                    <Button asChild>
+                        <Link href="/employees">View all</Link>
+                    </Button>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-base">Employee present</CardTitle>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-base">Employee of the month</CardTitle>
+                </CardHeader>
+            </Card>
         </div>
     )
 }
