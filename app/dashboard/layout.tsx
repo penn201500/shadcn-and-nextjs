2d920416deba098f211ff6cb653ad35840713e3c
penn <penn201500@gmail.com>
2025-03-03 17:12:08 +0800

    ✨ Make sure mobile main menu will be hidden if the screen size updated
    true

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,30 @@
+"use client"
+
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
 import MainMenu from "./components/main-menu"
 import MenuTitle from "./components/menu-title"
 import { MenuIcon } from "lucide-react"
+import { useMediaQuery } from "../hooks/use-media-query"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const isDesktop = useMediaQuery("(min-width: 768px)")
+
     return (
         <div className="grid md:grid-cols-[250px_1fr] h-screen">
             <MainMenu className="hidden md:flex" />
-            <div className="flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border p-4">
-                <MenuTitle />
-                <Drawer>
-                    <DrawerTrigger>
-                        <MenuIcon />
-                    </DrawerTrigger>
-                    <DrawerContent>
-                        <MainMenu />
-                    </DrawerContent>
-                </Drawer>
-            </div>
+            {!isDesktop && (
+                <div className="flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border p-4">
+                    <MenuTitle />
+                    <Drawer>
+                        <DrawerTrigger>
+                            <MenuIcon />
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <MainMenu />
+                        </DrawerContent>
+                    </Drawer>
+                </div>
+            )}
             <div className="overflow-auto py-2 px-4">
                 <h1>Welcome back!</h1>
                 {children}
