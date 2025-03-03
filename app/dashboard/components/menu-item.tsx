038a49d7c1bdf17d4075ad8ed20c4e2ebeda39a0
penn <penn201500@gmail.com>
2025-03-03 21:55:36 +0800

    ✨ Update drawer context to make sure mobile main menu will automatically close after clicking menu items
    true

--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -3,6 +3,8 @@
 import { cn } from "@/lib/utils"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useContext } from "react"
+import { DrawerContext } from "@/components/ui/drawer"
 
 type Props = {
     children: React.ReactNode
@@ -10,11 +12,20 @@ type Props = {
 }
 
 export default function MenuItem({ children, href }: Props) {
+    const { onClose } = useContext(DrawerContext)
     const pathname = usePathname()
     const isActive = pathname === href
     return (
         <Link
-            className={cn('block hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground', isActive && 'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-foreground text-foreground', 'h-12',  'flex items-center' )} href={href}>
+            className={cn(
+                "block hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground",
+                isActive &&
+                    "bg-primary hover:bg-primary dark:hover:bg-primary hover:text-foreground text-foreground",
+                "h-12",
+                "flex items-center"
+            )}
+            href={href}
+            onClick={onClose}>
             {children}
         </Link>
     )
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,9 +5,11 @@ import MainMenu from "./components/main-menu"
 import MenuTitle from "./components/menu-title"
 import { MenuIcon } from "lucide-react"
 import { useMediaQuery } from "../hooks/use-media-query"
+import { useState } from "react"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     const isDesktop = useMediaQuery("(min-width: 768px)")
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
         <div className="md:grid md:grid-cols-[250px_1fr] h-screen">
@@ -15,7 +17,11 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
             {!isDesktop && (
                 <div className="flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border p-4">
                     <MenuTitle />
-                    <Drawer direction="right">
+                    <Drawer
+                        direction="right"
+                        open={mobileMenuOpen}
+                        onClose={() => setMobileMenuOpen(false)}
+                        onOpenChange={open => setMobileMenuOpen(open)}>
                         <DrawerTrigger>
                             <MenuIcon />
                         </DrawerTrigger>
