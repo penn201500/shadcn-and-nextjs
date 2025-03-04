884e14bee28949df26e6cfb5f0067f37d7f09071
penn <penn201500@gmail.com>
2025-03-04 20:55:52 +0800

    ✨ Render avatar in employees data table cell
    true

--- app/dashboard/employees/columns.tsx
+++ app/dashboard/employees/columns.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import { Avatar } from "@/components/ui/avatar"
+import { AvatarFallback } from "@radix-ui/react-avatar"
 import { ColumnDef } from "@tanstack/react-table"
+import Image from "next/image"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -14,7 +17,34 @@ export type Employee = {
 }
 
 export const columns: ColumnDef<Employee>[] = [
-    { accessorKey: "avatar", header: "" },
+    {
+        accessorKey: "avatar",
+        header: "",
+        cell: ({ row }) => {
+            const avatar: string = row.getValue("avatar")
+            const firstName: string = row.getValue("firstName")
+            const lastName: string = row.getValue("lastName")
+
+            return (
+                <Avatar className="w-10 h-10">
+                    {!!avatar && (
+                        <Image
+                            src={avatar}
+                            alt={`${firstName} ${lastName}`}
+                            width={40}
+                            height={40}
+                            className="rounded-full object-cover"
+                        />
+                    )}
+                    {/* <AvatarImage src={avatar} alt={`${firstName} ${lastName}`} /> */}
+                    <AvatarFallback className="h-full w-full rounded-full flex items-center justify-center uppercase">
+                        {firstName[0]}
+                        {lastName[0]}
+                    </AvatarFallback>
+                </Avatar>
+            )
+        },
+    },
     { accessorKey: "firstName", header: "First Name" },
     { accessorKey: "lastName", header: "Last Name" },
     { accessorKey: "teamName", header: "Team" },
