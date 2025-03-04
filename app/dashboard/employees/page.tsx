1e8d9c40ea711fb00103c8e8e476bc35b94d295e
penn <penn201500@gmail.com>
2025-03-04 15:59:42 +0800

    ✨ Create the employees data table
    true

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,7 +1,88 @@
-export default function EmployeesPage() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { DataTable } from "@/components/ui/data-table"
+import { setTimeout } from "timers/promises"
+import { columns, type Employee } from "./columns"
+export default async function EmployeesPage() {
+    await setTimeout(5000)
+    const employees: Employee[] = [
+        {
+            id: 1,
+            firstName: "Colin",
+            lastName: "Murray",
+            teamName: "alpha",
+            isTeamLeader: true,
+            avatar: "/images/cm.jpg",
+        },
+        {
+            id: 2,
+            firstName: "Tom",
+            lastName: "Phillips",
+            teamName: "alpha",
+            isTeamLeader: false,
+        },
+        {
+            id: 3,
+            firstName: "Liam",
+            lastName: "Fuentes",
+            teamName: "alpha",
+            isTeamLeader: false,
+        },
+        {
+            id: 4,
+            firstName: "Tina",
+            lastName: "Fey",
+            teamName: "canary",
+            isTeamLeader: true,
+            avatar: "/images/tf.jpg",
+        },
+        {
+            id: 5,
+            firstName: "Katie",
+            lastName: "Johnson",
+            teamName: "canary",
+            isTeamLeader: false,
+        },
+        {
+            id: 6,
+            firstName: "Tina",
+            lastName: "Jones",
+            teamName: "canary",
+            isTeamLeader: false,
+        },
+        {
+            id: 7,
+            firstName: "Amy",
+            lastName: "Adams",
+            teamName: "delta",
+            isTeamLeader: true,
+        },
+        {
+            id: 8,
+            firstName: "Ryan",
+            lastName: "Lopez",
+            teamName: "delta",
+            isTeamLeader: false,
+            avatar: "/images/rl.jpg",
+        },
+        {
+            id: 9,
+            firstName: "Jenny",
+            lastName: "Jones",
+            teamName: "delta",
+            isTeamLeader: false,
+        },
+    ]
     return (
-        <div className="flex flex-col gap-4">
-            <p>Employees</p>
-        </div>
+        <Card>
+            <CardTitle>Employees</CardTitle>
+            <CardHeader>
+                <CardContent>
+                    <DataTable
+                        columns={columns}
+                        data={employees}
+                    />
+                </CardContent>
+            </CardHeader>
+        </Card>
     )
 }
